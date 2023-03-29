49aed89420b601046735bf14e6e9ad1cdf4329ec
Marcin Izdebski <mizdebski123@gmail.com>
2023-03-29 14:36:20 +0200

    Added search and home folder

---
+++ src/common/Home/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Search } from './Search/search';
+import { Wrapper } from './styledHome';
+
+export const Home = () => {
+    return (
+        <Wrapper>
+            <Search />
+        </Wrapper>
+    );
+};
+
