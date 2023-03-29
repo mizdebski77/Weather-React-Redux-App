6b3d91497570f568b0917071b912bc85d5ac82ff
Marcin Izdebski <mizdebski123@gmail.com>
2023-03-29 15:00:24 +0200

    Added bascics styles for home

--- src/common/Home/styledHome.js
+++ src/common/Home/styledHome.js
@@ -2,5 +2,8 @@ import styled from "styled-components";
 
 export const Wrapper = styled.main`
     max-width: 1200px;
+    min-height: 80vh;
+    padding: 50px;
     margin: 0 auto;
+    box-shadow: 0 0 30px #226586;
 `;
