0180edeba873c10fc723b68dea91fc0840db3a44
Marcin Izdebski <mizdebski123@gmail.com>
2023-04-03 16:29:33 +0200

    Added slice for search

--- src/common/Home/Search/searchSlice.js
+++ src/common/Home/Search/searchSlice.js
@@ -3,14 +3,13 @@ import { createSlice } from "@reduxjs/toolkit";
 const searchSlice = createSlice({
     name: "location",
     initialState: {
-        cityName: "London"
+        cityName: "London",
     },
-
     reducers: {
         setLocation: (state, action) => {
             state.cityName = action.payload;
-        }
-    }
+        },
+    },
 });
 
 export const { setLocation } = searchSlice.actions;
--- src/core/store.js
+++ src/core/store.js
@@ -2,14 +2,14 @@ import createSagaMiddleware from "@redux-saga/core";
 import { configureStore } from "@reduxjs/toolkit";
 import weatherReducer from '../common/Home/FetchWeather/weatherSlice'
 import rootSaga from "./rootSaga";
-import locationReducer from '../common/Home/Search/searchSlice'
+import searchReducer from '../common/Home/Search/searchSlice'
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         weather: weatherReducer,
-        location: locationReducer,
+        search: searchReducer,
     },
     middleware: [sagaMiddleware],
 });
