5f92a0b7d163ccb85c42111cc712b5beffafb1fb
Marcin Izdebski <mizdebski123@gmail.com>
2023-04-04 14:27:53 +0200

    Removed min height

--- src/common/Home/styledHome.js
+++ src/common/Home/styledHome.js
@@ -3,7 +3,6 @@ import SVG from "react-inlinesvg";
 
 export const Wrapper = styled.main`
     max-width: 1000px;
-    min-height: 90vh;
     padding: 50px;
     margin: 0 auto;
     box-shadow: 0 0 30px #ffffff;
